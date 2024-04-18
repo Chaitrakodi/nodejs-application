f8d77ee96d5ab2f5b7cffe579c4f6abae5a28671
chaitra <chaitrak021998@gmail.com>
2024-04-18 16:53:06 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const PORT = 3001;
 // basic routes
 router(app);
 app.get("/", (req, res) => {
-   res.send("<h1> Hello World </h1>");
+   res.send("<h1> Hello Chaitra </h1>");
 });
 
 // middleware
